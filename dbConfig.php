ca34858922dcbd66d970ca9fa11aa76d283a67c8
KITAKA GRACE <34123938+prophetgrace@users.noreply.github.com>
2020-04-17 11:58:17 +0300

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername  = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = "testhouse";
+$servername  = 'us-cdbr-iron-east-04.cleardb.net';
+$username = 'b1b46812a8f310';
+$password = '226540aa';
+$dbname = "heroku_184a789d3d8c2d7";
 
 $connect  = mysqli_connect($servername,$username,$password,$dbname);
 
 if ($connect) {
-  //  echo "Sucessfull connection";
+    echo "Sucessfull connection";
 } else {
     echo "System is not connected to database";
 }
